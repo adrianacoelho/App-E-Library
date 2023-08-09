dab0de6045fe8590873cc48f9729cea1fbf344c2
adrianacoelho <108436895+adrianacoelho@users.noreply.github.com>
2023-08-09 12:15:40 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+    return <BottomTabNavigator />;
+  }
+}
+
+/*import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); */
